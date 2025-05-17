a7107da976e376963a4cf0a224c06718eb6adff7
Janielrodz <133167602+Janielrodz@users.noreply.github.com>
2025-05-17 13:00:00 -0400

    pdf

--- final_project/config.py
+++ final_project/config.py
@@ -1,12 +1,12 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv() #carga automáticamente las variables de entorno definidas en un archivo .env. 
 
 #Variables de entorno
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY') #Carga el SECRET_KEY ya establecido en otro archivo.
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') #Carga el DATABASE_URL desde otro archivo.
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
